00a328f91d07a73f4c98c0969d38e937e36b5026
SlimTim10 <slimtim10@gmail.com>
2015-02-17 09:53:09 -0500

    Fix interrupt flags and handle invalid pin.

--- simple_buttons.c
+++ simple_buttons.c
@@ -15,21 +15,22 @@ volatile boolean *d3flag = false;
 
 /* Set flag to true when interrupt on external pin is triggered */
 void simple_interrupt(uint8_t pin, uint8_t trig, volatile void *flag) {
-	d2flag = flag;
-
 	switch (pin) {
 	case 2:
 		/* D2 */
+		d2flag = flag;
 		EIFR &= ~bit(INTF0);	/* Clear D2 interrupt flag */
-		EICRA &= ~bit(ISC00);	/* Clear control register */
-		EICRA &= ~bit(ISC01);
 		switch (trig) {
 		case LOW:
+			EICRA &= ~bit(ISC00);	/* Trigger on low */
+			EICRA &= ~bit(ISC01);
 			break;
 		case CHANGE:
 			EICRA |= bit(ISC00);	/* Trigger on change */
+			EICRA &= ~bit(ISC01);
 			break;
 		case FALLING:
+			EICRA &= ~bit(ISC00);
 			EICRA |= bit(ISC01);	/* Trigger on falling edge */
 			break;
 		case RISING:
@@ -40,16 +41,19 @@ void simple_interrupt(uint8_t pin, uint8_t trig, volatile void *flag) {
 		break;
 	case 3:
 		/* D3 */
+		d3flag = flag;
 		EIFR &= ~bit(INTF1);	/* Clear D3 interrupt flag */
-		EICRA &= ~bit(ISC10);	/* Clear control register */
-		EICRA &= ~bit(ISC11);
 		switch (trig) {
 		case LOW:
+			EICRA &= ~bit(ISC10);	/* Trigger on low */
+			EICRA &= ~bit(ISC11);
 			break;
 		case CHANGE:
 			EICRA |= bit(ISC10);	/* Trigger on change */
+			EICRA &= ~bit(ISC11);
 			break;
 		case FALLING:
+			EICRA &= ~bit(ISC10);
 			EICRA |= bit(ISC11);	/* Trigger on falling edge */
 			break;
 		case RISING:
@@ -58,6 +62,8 @@ void simple_interrupt(uint8_t pin, uint8_t trig, volatile void *flag) {
 		}
 		EIMSK |= bit(INT1);	/* Enable INT0 interrupt */
 		break;
+	default:
+		return;
 	}
 
 	/* Enable interrupts */
@@ -75,5 +81,5 @@ ISR(INT0_vect) {
 }
 
 ISR(INT1_vect) {
-	*d2flag = true;
+	*d3flag = true;
 }
