b72aa6c20c972620ae7d9858aa5d94399e65d59f
SlimTim10 <slimtim10@gmail.com>
2015-02-16 20:40:47 -0500

    Add functionality for D3 interrupt.

--- simple_buttons.c
+++ simple_buttons.c
@@ -7,6 +7,7 @@
 */
 
 volatile boolean *d2flag = false;
+volatile boolean *d3flag = false;
 
 void simple_interrupt(uint8_t pin, uint8_t trig, volatile void *flag) {
 	d2flag = flag;
@@ -32,6 +33,26 @@ void simple_interrupt(uint8_t pin, uint8_t trig, volatile void *flag) {
 		}
 		EIMSK |= bit(INT0);	/* Enable INT0 interrupt */
 		break;
+	case 3:
+		/* D3 */
+		EIFR &= ~bit(INTF1);	/* Clear D3 interrupt flag */
+		EICRA &= ~bit(ISC10);	/* Clear control register */
+		EICRA &= ~bit(ISC11);
+		switch (trig) {
+		case LOW:
+			break;
+		case CHANGE:
+			EICRA |= bit(ISC10);	/* Trigger on change */
+			break;
+		case FALLING:
+			EICRA |= bit(ISC11);	/* Trigger on falling edge */
+			break;
+		case RISING:
+			EICRA |= bit(ISC10) | bit(ISC11);	/* Trigger on rising edge */
+			break;
+		}
+		EIMSK |= bit(INT1);	/* Enable INT0 interrupt */
+		break;
 	}
 
 	/* Enable interrupts */
@@ -44,5 +65,9 @@ void simple_debounce(uint8_t pin, boolean state) {
 }
 
 ISR(INT0_vect) {
+	*d2flag = true;
+}
+
+ISR(INT1_vect) {
 	*d2flag = true;
 }
